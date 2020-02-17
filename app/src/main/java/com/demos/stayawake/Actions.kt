d38a9f235fd432d23d8d9cf29bd240419ba0a8a2
hjgode <hjgode@gmail.com>
2020-02-17 15:39:32 +0100

    Initial commit

---
+++ app/src/main/java/com/demos/stayawake/Actions.kt
@@ -0,0 +1,6 @@
+package com.demos.stayawake
+
+enum class Actions {
+    START,
+    STOP
+}
---
+++ app/src/main/java/com/demos/stayawake/Utils.kt
@@ -0,0 +1,7 @@
+package com.demos.stayawake
+
+import android.util.Log
+
+fun log(msg: String) {
+    Log.d("ENDLESS-SERVICE", msg)
+}
